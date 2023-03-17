556100f663f1e33a12705da565065eed782d336b
sanjeevidev <sanjeevikumar116@gmail.com>
2023-03-17 17:11:10 +0530

    Added Longest Palindrome

--- C++/Longest Palindromic Substring.cpp
+++ C++/Longest Palindromic Substring.cpp
@@ -1,33 +1,68 @@
-Longest Palindromic Substring
-
-//code
 
 #include <bits/stdc++.h>
 using namespace std;
 
-void printSubStr(string str, int low, int high)
+// Function to print a substring
+// str[low..high]
+void printSubStr(
+	string str, int low, int high)
 {
 	for (int i = low; i <= high; ++i)
 		cout << str[i];
 }
 
+// This function prints the
+// longest palindrome substring
+// It also returns the length of
+// the longest palindrome
 int longestPalSubstr(string str)
 {
+	// get length of input string
 	int n = str.size();
 
-	int maxLength = 1, start = 0;
+	// table[i][j] will be false if substring
+	// str[i..j] is not palindrome.
+	// Else table[i][j] will be true
+	bool table[n][n];
 
-	for (int i = 0; i < str.length(); i++) {
-		for (int j = i; j < str.length(); j++) {
-			int flag = 1;
+	memset(table, 0, sizeof(table));
 
-			for (int k = 0; k < (j - i + 1) / 2; k++)
-				if (str[i + k] != str[j - k])
-					flag = 0;
+	// All substrings of length 1
+	// are palindromes
+	int maxLength = 1;
 
-			if (flag && (j - i + 1) > maxLength) {
-				start = i;
-				maxLength = j - i + 1;
+	for (int i = 0; i < n; ++i)
+		table[i][i] = true;
+
+	// check for sub-string of length 2.
+	int start = 0;
+	for (int i = 0; i < n - 1; ++i) {
+		if (str[i] == str[i + 1]) {
+			table[i][i + 1] = true;
+			start = i;
+			maxLength = 2;
+		}
+	}
+
+	// Check for lengths greater than 2.
+	// k is length of substring
+	for (int k = 3; k <= n; ++k) {
+		// Fix the starting index
+		for (int i = 0; i < n - k + 1; ++i) {
+			// Get the ending index of substring from
+			// starting index i and length k
+			int j = i + k - 1;
+
+			// checking for sub-string from ith index to
+			// jth index if str[i+1] to str[j-1] is a
+			// palindrome
+			if (table[i + 1][j - 1] && str[i] == str[j]) {
+				table[i][j] = true;
+
+				if (k > maxLength) {
+					start = i;
+					maxLength = k;
+				}
 			}
 		}
 	}
@@ -35,9 +70,11 @@ int longestPalSubstr(string str)
 	cout << "Longest palindrome substring is: ";
 	printSubStr(str, start, start + maxLength - 1);
 
+	// return length of LPS
 	return maxLength;
 }
 
+// Driver Code
 int main()
 {
 	string str = "forgeeksskeegfor";
